c7018619b25b56bb5f4a99224f5cc5fb92b00190
Gerpar <Gerpar.gerps@gmail.com>
2021-07-11 19:32:41 -0400

    Polished up the VTOL controls and added some basic VFX
    VFX changes colors whenever the VTOL thruster it's bound to is activated. (Done through the VtolControls script.

--- Unity Files/Pompous Trash Game Jam 2021/Assets/_Scripts/VtolControls.cs
+++ Unity Files/Pompous Trash Game Jam 2021/Assets/_Scripts/VtolControls.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class VtolControls : MonoBehaviour
 {
+    enum Thruster { Top_Left, Top_Right, Bot_Left, Bot_Right};
+
     [SerializeField] GameObject thrusterTL, thrusterTR, thrusterBL, thrusterBR, pitchUp, pitchDown, rollRight, rollLeft;
     [SerializeField] float thrusterForce;
 
@@ -11,12 +13,34 @@ public class VtolControls : MonoBehaviour
     [SerializeField][Tooltip("How many degrees the VTOL can tip before stabilizers take effect")] float maxTilt = 45;
     [SerializeField] float minHeight = 1.0f;
     [SerializeField] float maxHeight = 5.0f;
+    [SerializeField] ParticleSystem thrusterEnabledParticles, thrusterDisabledParticles;
 
     Rigidbody rb;
+
+    // TTL = Thruster Top Left
+    // TTR = Thruster Top Right
+    // TBL = Thruster Bottom Left
+    // TBR = Thruster Bottom Right
+    //Particle variables
+    ParticleSystem.MainModule TTLMain, TTRMain, TBLMain, TBRMain;
+    ParticleSystem.ColorOverLifetimeModule TTLCOL, TTRCOL, TBLCOL, TBRCOL;
+
+    // Thruster enabled states
+    bool TTLEnabled, TTREnabled, TBLEnabled, TBREnabled;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        TTLMain = thrusterTL.transform.GetChild(0).GetComponent<ParticleSystem>().main;
+        TTRMain = thrusterTR.transform.GetChild(0).GetComponent<ParticleSystem>().main;
+        TBLMain = thrusterBL.transform.GetChild(0).GetComponent<ParticleSystem>().main;
+        TBRMain = thrusterBR.transform.GetChild(0).GetComponent<ParticleSystem>().main;
+
+        TTLCOL = thrusterTL.transform.GetChild(0).GetComponent<ParticleSystem>().colorOverLifetime;
+        TTRCOL = thrusterTR.transform.GetChild(0).GetComponent<ParticleSystem>().colorOverLifetime;
+        TBLCOL = thrusterBL.transform.GetChild(0).GetComponent<ParticleSystem>().colorOverLifetime;
+        TBRCOL = thrusterBR.transform.GetChild(0).GetComponent<ParticleSystem>().colorOverLifetime;
     }
 
     // Update is called once per frame
@@ -36,18 +60,41 @@ public class VtolControls : MonoBehaviour
         if (Input.GetButton("ThrusterTopLeft"))
         {
             rb.AddForceAtPosition(0.5f * thrusterForce * -thrusterTL.transform.forward, thrusterTL.transform.position);
+            if (!TTLEnabled) ToggleThruster(Thruster.Top_Left);
         }
+        else
+        {
+            if (TTLEnabled) ToggleThruster(Thruster.Top_Left);
+        }
+
         if (Input.GetButton("ThrusterTopRight"))
         {
             rb.AddForceAtPosition(0.5f * thrusterForce * -thrusterTR.transform.forward, thrusterTR.transform.position);
+            if (!TTREnabled) ToggleThruster(Thruster.Top_Right);
         }
+        else
+        {
+            if (TTREnabled) ToggleThruster(Thruster.Top_Right);
+        }
+
         if (Input.GetButton("ThrusterBotLeft"))
         {
             rb.AddForceAtPosition(0.5f * thrusterForce * -thrusterBL.transform.forward, thrusterBL.transform.position);
+            if (!TBLEnabled) ToggleThruster(Thruster.Bot_Left);
         }
+        else
+        {
+            if (TBLEnabled) ToggleThruster(Thruster.Bot_Left);
+        }
+
         if (Input.GetButton("ThrusterBotRight"))
         {
             rb.AddForceAtPosition(0.5f * thrusterForce * -thrusterBR.transform.forward, thrusterBR.transform.position);
+            if (!TBREnabled) ToggleThruster(Thruster.Bot_Right);
+        }
+        else
+        {
+            if (TBREnabled) ToggleThruster(Thruster.Bot_Right);
         }
     }
 
@@ -104,6 +151,81 @@ public class VtolControls : MonoBehaviour
         else if(transform.position.y > maxHeight)
         {
             rb.AddForce(-Vector3.up * (thrusterForce * 2));
+        }
+    }
+
+    /// <summary>
+    /// Updates particle effects attached to thruster
+    /// </summary>
+    /// <param name="thrusterToToggle"></param>
+    void ToggleThruster(Thruster thrusterToToggle)
+    {
+        switch (thrusterToToggle)
+        {
+            case Thruster.Top_Left:
+                TTLEnabled = !TTLEnabled;
+
+                if(TTLEnabled)
+                {
+                    // Change particles to be on
+                    TTLMain.startColor = thrusterEnabledParticles.main.startColor;
+                    TTLCOL.color = thrusterEnabledParticles.colorOverLifetime.color;
+                }
+                else
+                {
+                    // Change particles to be off
+                    TTLMain.startColor = thrusterDisabledParticles.main.startColor;
+                    TTLCOL.color = thrusterDisabledParticles.colorOverLifetime.color;
+                }
+                break;
+            case Thruster.Top_Right:
+                TTREnabled = !TTREnabled;
+
+                if (TTREnabled)
+                {
+                    // Change particles to be on
+                    TTRMain.startColor = thrusterEnabledParticles.main.startColor;
+                    TTRCOL.color = thrusterEnabledParticles.colorOverLifetime.color;
+                }
+                else
+                {
+                    // Change particles to be off
+                    TTRMain.startColor = thrusterDisabledParticles.main.startColor;
+                    TTRCOL.color = thrusterDisabledParticles.colorOverLifetime.color;
+                }
+                break;
+            case Thruster.Bot_Left:
+                TBLEnabled = !TBLEnabled;
+
+                if (TBLEnabled)
+                {
+                    // Change particles to be on
+                    TBLMain.startColor = thrusterEnabledParticles.main.startColor;
+                    TBLCOL.color = thrusterEnabledParticles.colorOverLifetime.color;
+                }
+                else
+                {
+                    // Change particles to be off
+                    TBLMain.startColor = thrusterDisabledParticles.main.startColor;
+                    TBLCOL.color = thrusterDisabledParticles.colorOverLifetime.color;
+                }
+                break;
+            case Thruster.Bot_Right:
+                TBREnabled = !TBREnabled;
+
+                if (TBREnabled)
+                {
+                    // Change particles to be on
+                    TBRMain.startColor = thrusterEnabledParticles.main.startColor;
+                    TBRCOL.color = thrusterEnabledParticles.colorOverLifetime.color;
+                }
+                else
+                {
+                    // Change particles to be off
+                    TBRMain.startColor = thrusterDisabledParticles.main.startColor;
+                    TBRCOL.color = thrusterDisabledParticles.colorOverLifetime.color;
+                }
+                break;
         }
     }
 }
