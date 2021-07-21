5a1131a937c65bc244b623bb200e1b2e12697533
Gerpar <Gerpar.gerps@gmail.com>
2021-07-20 20:22:21 -0400

    Fixed bugs with the magnet regarding kill planes and goal pads

--- Unity Files/Pompous Trash Game Jam 2021/Assets/_Scripts/KillPlane.cs
+++ Unity Files/Pompous Trash Game Jam 2021/Assets/_Scripts/KillPlane.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class KillPlane : MonoBehaviour
 {
+    [SerializeField] MagnetController magnetScript;
     private void OnCollisionEnter(Collision collision)
     {
        if (collision.gameObject.layer == 6)
         {
+            magnetScript.RemoveObject(collision.collider);
             collision.gameObject.transform.position = collision.gameObject.GetComponent<NeoTrash>().startPos;
             collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
